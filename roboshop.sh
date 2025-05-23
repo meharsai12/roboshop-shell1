bfb6a4c4592000feb17ec4889ea32f26cd99a48c
Ravikanti Sai <RavikantiS@emids.com>
2025-05-23 20:09:32 +0530

    args

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ INSTANCES=("frontend" "mongodb" "catalogue" "mysql" "redis" "cart" "user" "rabbi
 ZONE_ID="Z087991113U7C6A5OTRJB"
 DOMAIN_NAME="meharsai.site"
 
-for instances in ${INSTANCES[@]}
+for instance in ${INSTANCES[@]}
 #for insatances in $@
 do 
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-04272fef9ce3ac807807 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
