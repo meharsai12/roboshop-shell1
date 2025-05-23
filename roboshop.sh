fd68f739bb81506b5f928826bce98554b8b47b3d
Ravikanti Sai <RavikantiS@emids.com>
2025-05-23 19:52:56 +0530

    args

--- roboshop.sh
+++ roboshop.sh
@@ -1,17 +1,16 @@
-#!bin/bash
-
+#!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-04272fef9ce3ac807"
-INSTANCES=("frontend" "mongodb" "catalogue" "mysql" "redis" "cart" "user" "rabbitmq" "shipping" "payment" "dispatch")
-ZONE_ID="Z087991113U7C6A5OTRJB"
-DOMAIN_NAME="meharsai.site"
+SG_ID="sg-01bc7ebe005fb1cb2" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
+DOMAIN_NAME="daws84s.site" # replace with your domain
 
-for instances in ${INSTANCES[@]}
-#for insatances in $@
-do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-04272fef9ce3ac807807 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance -ne "frontend" ]
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
@@ -21,25 +20,21 @@ do
     fi
     echo "$instance IP address: $IP"
 
-
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
-            "Comment": "Creating a record set for cognito endpoint"
-            ,"Changes": [{
-            "Action"              : "UPSERT"
-            ,"ResourceRecordSet"  : {
-                "Name"              : "'$instances'.'$DOMAIN_NAME'"
-                ,"Type"             : "A"
-                ,"TTL"              : 1
-                ,"ResourceRecords"  : [{
-                    "Value"         : "'$IP'"
-                }]
-            }
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
             }]
+        }
+        }]
     }'
-
-
-
 done
