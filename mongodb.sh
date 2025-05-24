264ec3bc7361c1ac78ae00a9cbb6de9c1071dcd3
Ravikanti Sai <RavikantiS@emids.com>
2025-05-24 15:33:19 +0530

    args

---
+++ mongodb.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "python" "nginx" "httpd")
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e " $R ERROR : you dont have root access to run the script  $N " | tee -a $LOG_FILE #by using tee command the output shown on screen and stored in logs also ,we have -e for echo to print colours and $N to stop the colour for that line 
+    exit 1
+else
+    echo -e "$R you have root access to run the script $N " | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "  $Y $2  is success $N " | tee -a $LOG_FILE
+    else
+        echo -e " $Y $2  is failure $N " | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying the mongodb repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? " Installing mongodb "
+
+systemctl enable mongod 
+VALIDATE $? "enable mongod"
+
+
+systemctl start mongod
+VALIDATE $? "start mongod" 
+
+
+sed -i 's/127.0.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $?  " replacin ip address for remote connections"
+
+systemctl restart mongod
+VALIDATE $? "Restarting mongodb"
