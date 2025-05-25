bf2067385bda1f3efeafa66cb6bc4435ea43b319
Ravikanti Sai <RavikantiS@emids.com>
2025-05-25 14:55:01 +0530

    args

--- catalogue.sh
+++ catalogue.sh
@@ -85,10 +85,10 @@ VALIDATE $? "mongodb.repo relading"
 dnf install mongodb-mongosh -y   &>>$LOG_FILE
 VALIDATE $? "installing mongodb client"
 
-STATUS=$(mongosh --host mongodb.meharsai.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host 172.31.38.69 --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.meharsai.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host 172.31.38.69 </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
