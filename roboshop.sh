23e38714b1685a7438a531b6ad0503c9e05f91ec
Ravikanti Sai <RavikantiS@emids.com>
2025-05-26 13:31:34 +0530

    args

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ ZONE_ID="Z087991113U7C6A5OTRJB"
 DOMAIN_NAME="meharsai.site"
 
 #for instance in ${INSTANCES[@]}
-for insatances in $@
+for instances in $@
 do 
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-04272fef9ce3ac807 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
