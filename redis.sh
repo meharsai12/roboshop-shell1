69b87cfc056d7c7fade98dca0464a926b7128498
Ravikanti Sai <RavikantiS@emids.com>
2025-05-26 14:22:14 +0530

    args

--- redis.sh
+++ redis.sh
@@ -34,4 +34,25 @@ VALIDATE(){
     fi
 }
 
+dnf module disable redis -y    &>>LOG_FILE
+VALIDATE $? "disabling redis"
 
+dnf module enable redis:7 -y  &>>LOG_FILE
+VALIDATE $? "enabling redis"
+
+dnf install redis -y   &>>LOG_FILE
+VALIDATE $? "installing redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0./g' -e  '/protected-mode/ c protected-mode no ' /etc/redis/redis.conf &>>LOG_FILE
+VALIDATE $? " configuring to accept remote connections"
+
+
+systemctl enable redis  &>>LOG_FILE
+VALIDATE $? "enabling redis"
+
+systemctl start redis  &>>LOG_FILE
+VALIDATE $? "starting redis "
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $START_TIME - $END_TIME ))
+echo -e " Time taken to execute script is :: $Y $TOTAL_TIME in seconds $N "
