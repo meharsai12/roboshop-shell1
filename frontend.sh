17b80e883f0fcddbf039f01bac306794e8aa5434
Ravikanti Sai <RavikantiS@emids.com>
2025-05-26 15:16:39 +0530

    args

--- frontend.sh
+++ frontend.sh
@@ -64,7 +64,7 @@ VALIDATE $? "unzipping frontend"
 rm -rf /etc/nginx/nginx.conf
 VALIDATE $? "removing default nginx conf"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.confg /etc/nginx/nginx.conf
 VALIDATE $? "nginxconfigurations "
 
 systemctl restart nginx 
