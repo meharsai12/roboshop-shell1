fabbb4c8fc942c2572b6b49182cc35077e51f8e5
Ravikanti Sai <RavikantiS@emids.com>
2025-05-26 14:37:37 +0530

    args

---
+++ user.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+PACKAGES=("mysql" "python" "nginx" "httpd")
+SCRIPT_DIR=$PWD
+
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e " $R ERROR : you dont have root access to run the script  $N " | tee -a $LOG_FILE #by using tee command the output shown on screen and stored in logs also ,we have -e for echo to print colours and $N to stop the colour for that line 
+    exit 1
+else
+    echo -e "$R you have root access to run the script $N " | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "   $2....  is $G success $N " | tee -a $LOG_FILE
+    else
+        echo -e "  $2....   is $Y failure $N " | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+
+dnf module disable nodejs -y  &>>$LOG_FILE
+VALIDATE $? " disabling the nodejs "
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? " enable the nodejs:20 "
+
+dnf install nodejs -y  &>>$LOG_FILE
+VALIDATE $? " installing the nodejs"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "creating roboshop user "
+else
+    echo -e "Roboshop user is already created.... $G SKIPPING $N "
+fi
+
+mkdir -p /app  #by adding -p , it created directory if no there , if there it dont create
+VALIDATE $? "creating app directory "
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading user zipfile "
+
+rm -rf /app/* #by adding this line if we run n number of times it wont stuck because if we alreadr run one time it asks again to say whether it is there so by giving this command it deletes the data present and unzip again 
+cd /app 
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "unzipping the user"
+
+npm install  &>>$LOG_FILE 
+VALIDATE $? "installing the dependicies "
+
+
